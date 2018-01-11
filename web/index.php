19ae764072c66de5723df2a587917a9c24a69934
surajsen <35251009+surajsen@users.noreply.github.com>
2018-01-11 18:17:47 +0530

    upload app php

--- web/index.php
+++ web/index.php
@@ -55,11 +55,11 @@ try {
 
 
     header('Content-Type: application/json; charset=utf-8');
-    /*header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: *');
 
 header('Access-Control-Allow-Methods: GET, POST');
 
-header("Access-Control-Allow-Headers: X-Requested-With");*/
+header("Access-Control-Allow-Headers: X-Requested-With");
 
     // return response
     die(json_encode(array(
